12994b5bda0ec9c1f7e60f6b31fe6a1a1aeb8edb
RCW2000 <70165226+RCW2000@users.noreply.github.com>
2024-03-02 18:17:19 -0500

    Update prompts.py

--- helpers/prompts.py
+++ helpers/prompts.py
@@ -54,7 +54,7 @@ def extractConcepts(prompt: str, metadata):
     return result
 
 
-def graphPrompt(input: str, metadata:Dict[T1, T2]):
+def graphPrompt(input: str, metadata:{}):
     print("hello")
 
     SYS_PROMPT = (
